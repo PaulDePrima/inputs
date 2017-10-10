5cae17f61f375d5dac8dfaaf2bd5f298567696d2
f.c.lopez <casmali.f.lopez@gmail.com>
2017-10-10 09:48:16 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,6 +8,9 @@ def render_main():
 @app.route("/fir")
 def render_page1():
     return render_template('first.html')
+@app.route("/sec")
+def render_page2():
+    return render_template('second.html')
 
 @app.route("/response")
 def render_response():
@@ -17,3 +20,11 @@ def render_response():
         #the way we get info from args is that it is visible in a url. - the information in args is visible in the url for hte page being requested(ex. .../response?color=blue)
         res = str(ins*2.54)
         return render_template('response.html', response = res)
+@app.route("/responsetwo")
+def render_responsetwo():
+        ins = float(request.args['inches'])
+        #The request object stores information that was sent by the client to the server.
+        #the args is a multidict
+        #the way we get info from args is that it is visible in a url. - the information in args is visible in the url for hte page being requested(ex. .../response?color=blue)
+        res = str(ins/12)
+        return render_template('responsetwo.html', response = res)

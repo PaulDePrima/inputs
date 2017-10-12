f2a89e2feb5d33e9e7de532a389dc63649a0a6b5
PaulDePrima <31520022+PaulDePrima@users.noreply.github.com>
2017-10-12 10:32:21 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -21,7 +21,7 @@ def render_response():
         #The request object stores information that was sent by the client to the server.
         #the args is a multidict
         #the way we get info from args is that it is visible in a url. - the information in args is visible in the url for hte page being requested(ex. .../response?color=blue)
-        res = str(ins*2.54)
+        res = str((ins-32)*5/9)
         return render_template('response.html', their = request.args['inches'], response = res)
 @app.route("/responsetwo")
 def render_responsetwo():
